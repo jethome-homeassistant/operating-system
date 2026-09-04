39ce3a7c028882e171a24a18fdb84ecbf78703a6
Nikita Maslo <nikitamalco203@gmail.com>
2026-09-04 13:35:52 +0300

    Update hash amlogic-boot-fip-j310

--- buildroot-external/package/amlogic-boot-fip-j310/amlogic-boot-fip-j310.mk
+++ buildroot-external/package/amlogic-boot-fip-j310/amlogic-boot-fip-j310.mk
@@ -5,7 +5,7 @@
 #
 ################################################################################
 
-AMLOGIC_BOOT_FIP_J310_VERSION = f1b7b257a7bff747e1b8d370919a3b43494a21a5
+AMLOGIC_BOOT_FIP_J310_VERSION = 0195df574f251c4bd97fa3a8a64aca503ddd3d56
 AMLOGIC_BOOT_FIP_J310_SITE = https://github.com/jethome-iot/amlogic-boot-fip.git
 AMLOGIC_BOOT_FIP_J310_SITE_METHOD = git
 AMLOGIC_BOOT_FIP_J310_INSTALL_IMAGES = YES
@@ -14,16 +14,6 @@ AMLOGIC_BOOT_FIP_J310_DEPENDENCIES = uboot
 AMLOGIC_BOOT_FIP_J310_LICENSE = PROPRIETARY
 AMLOGIC_BOOT_FIP_J310_REDISTRIBUTE = NO
 
-# NOTE: this list is deliberately package-prefixed. Do NOT reuse the unprefixed
-# AMLOGIC_BOOT_BINS variable owned by the amlogic-boot-fip-e package: both .mk
-# files are parsed unconditionally by external.mk, so a shared '+=' variable
-# would leak this package's file list into amlogic-boot-fip-e (and vice versa)
-# and break the j80/j100/j200 image install step.
-#
-# The s7.inc rule for ${O}/u-boot.bin produces all three files below:
-#   u-boot.bin        - raw eMMC/SPI bootloader image
-#   u-boot.bin.sd.bin - SD/eMMC image with the leading AMLBOOT payload sector
-#   u-boot.bin.usb    - USB (boot-from-USB / burn mode) bootloader image
 AMLOGIC_BOOT_FIP_J310_BINS = u-boot.bin u-boot.bin.sd.bin u-boot.bin.usb
 
 define AMLOGIC_BOOT_FIP_J310_BUILD_CMDS
@@ -35,7 +25,7 @@ endef
 ifeq ($(BR2_PACKAGE_AMLOGIC_BOOT_FIP_J310),y)
 ifeq ($(call qstrip,$(BR2_PACKAGE_AMLOGIC_BOOT_FIP_J310_BOARD)),)
 $(error No board u-boot firmware config name specified, check your BR2_PACKAGE_AMLOGIC_BOOT_FIP_J310_BOARD setting)
-endif # qstrip BR2_PACKAGE_AMLOGIC_BOOT_FIP_J310_BOARD
+endif
 endif
 
 define AMLOGIC_BOOT_FIP_J310_INSTALL_IMAGES_CMDS
