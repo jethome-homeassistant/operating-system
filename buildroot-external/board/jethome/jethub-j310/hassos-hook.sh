f98dd344dbd2cab704f8f2135e44c165c27edb49
Nikita Maslo <nikitamalco203@gmail.com>
2026-09-10 11:10:13 +0300

    Add j310 d3-mini support

--- buildroot-external/board/jethome/jethub-j310/hassos-hook.sh
+++ buildroot-external/board/jethome/jethub-j310/hassos-hook.sh
@@ -6,9 +6,15 @@ function haos_pre_image() {
 
     cp "${BINARIES_DIR}/boot.scr" "${BOOT_DATA}/boot.scr"
     mkdir -p "${BOOT_DATA}/amlogic"
-    cp "${BINARIES_DIR}/meson-axg-jethome-jethub-j100.dtb" "${BOOT_DATA}/amlogic/"
-    cp "${BINARIES_DIR}/meson-axg-jethome-jethub-j110-rev-2.dtb" "${BOOT_DATA}/amlogic/"
-    cp "${BINARIES_DIR}/meson-axg-jethome-jethub-j110-rev-3.dtb" "${BOOT_DATA}/amlogic/"
+
+    local dtb
+    dtb="$(find "${BUILD_DIR}"/linux-*/common_drivers/arch/arm64/boot/dts/amlogic \
+        -name "${KERNEL_DTB}" -print -quit 2>/dev/null)"
+    if [ -z "${dtb}" ]; then
+        echo "ERROR: ${KERNEL_DTB} not found in the kernel build tree"
+        return 1
+    fi
+    cp "${dtb}" "${BOOT_DATA}/amlogic/"
 
     if ls "${BINARIES_DIR}"/*.dtbo 1> /dev/null 2>&1; then
         echo "Found .dtbo files in ${BINARIES_DIR}"
@@ -16,7 +22,6 @@ function haos_pre_image() {
         cp "${BINARIES_DIR}"/*.dtbo "${BOOT_DATA}/overlays/"
     fi
     cp "${BOARD_DIR}/boot-env.txt" "${BOOT_DATA}/hassos-config.txt" || true
-    cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
 }
 
 function haos_post_image() {
